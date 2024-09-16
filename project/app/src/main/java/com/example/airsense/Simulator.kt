d47e8a949f2caf2d7100d8950265915d07409c1f
centauridesigns <zianisouhail@hotmail.com>
2024-09-16 11:15:29 +0200

    add file structure

---
+++ project/app/src/main/java/com/example/airsense/Simulator.kt
@@ -0,0 +1,24 @@
+package com.example.airsense
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+
+class Simulator(private val sensorManager: SensorManager, private val data: List<String>, private val resampleRate: Int) {
+
+    private val sensorMap = mutableMapOf<String, Sensor>()
+
+
+    private fun generateSensorEvent(s: String) {
+
+    }
+
+    private fun runSimulation() {
+
+    }
+
+    private fun resampleRecording() {
+
+    }
+}
+
